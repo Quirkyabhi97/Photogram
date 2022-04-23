4b5fe0b09db0c8d816f62e5b1406d7b5a0b02629
Abhinav Sharma <abhi28121997@gmail.com>
2022-04-23 23:21:49 +0530

    conditional className function added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,43 @@ import React, { Component } from "react";
 class Counter extends Component {
 
   state = {
-      count : 0
+      count : 1,
+      tags : ['tag1','tag2','tag3'] 
   };
 
+  
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.state.count}</span>
+        <button className="btn btn-secondary btn-sm" onClick={this.IncrementCount}>Increment</button>
+        <ul>
+          {this.state.tags.map(item=>
+            <li key={item}>{item}</li>
+          )}
+        </ul>
       </React.Fragment> 
     );
+  }
+
+  //conditional rendering example
+  
+
+ //on click of button handled here
+  IncrementCount(){
+    let {count}=this.state;
+    
+    console.log(this.count);
+    return count;
+   //return this.count;
+   
+  }
+
+ //conditional className example
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
