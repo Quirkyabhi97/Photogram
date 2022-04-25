f8e01b2c66645ed6360190097c11957d76b5b1ff
Abhinav Sharma <abhi28121997@gmail.com>
2022-04-25 13:37:02 +0530

    Condtional Component Rendering applied

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,53 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
   state = {
-      count : 1,
-      tags : ['tag1','tag2','tag3'] 
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
-  
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.state.count}</span>
-        <button className="btn btn-secondary btn-sm" onClick={this.IncrementCount}>Increment</button>
-        <ul>
-          {this.state.tags.map(item=>
-            <li key={item}>{item}</li>
-          )}
-        </ul>
-      </React.Fragment> 
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={this.IncrementCount}
+        >
+          Increment
+        </button>
+
+        <span>{this.renderTags()}</span>
+      </React.Fragment>
     );
   }
 
   //conditional rendering example
-  
-
- //on click of button handled here
-  IncrementCount(){
-    let {count}=this.state;
-    
-    console.log(this.count);
-    return count;
-   //return this.count;
-   
+  renderTags() {
+    if (this.state.tags.length === 0) return <h1>There are No Tags !</h1>;
+    else
+      return (
+        <div>
+          <h1>{this.state.tags.length} Tags Found !</h1>
+          <ul>
+            {this.state.tags.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      );
   }
 
- //conditional className example
+  //on click of button handled here
+  IncrementCount() {
+    let { count } = this.state;
+
+    console.log(this.count);
+    return count;
+    //return this.count;
+  }
+
+  //conditional className example
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
     classes += this.state.count === 0 ? "warning" : "primary";
